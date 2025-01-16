478491b1961112abd1d76345f1b4695e5ecbe961
kevinboehmisch <150022344+kevinboehmisch@users.noreply.github.com>
2025-01-16 10:41:19 +0100

    Create ausgabe_aller_tabellen.sql

---
+++ Softwaretechnik/Datenbanken_2/Lösungen_Laboraufgaben/ausgabe_aller_tabellen.sql
@@ -0,0 +1,7 @@
+SELECT * FROM dbo.Boehmisch_grund;
+SELECT * FROM dbo.Boehmisch_preisgeld;
+SELECT * FROM dbo.Boehmisch_spieler;
+SELECT * FROM dbo.Boehmisch_strafe;
+SELECT * FROM dbo.Boehmisch_team;
+SELECT * FROM dbo.Boehmisch_wettkampf;
+SELECT * FROM dbo.Boehmisch_wettkampf_log;
