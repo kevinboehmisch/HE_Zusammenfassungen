d6978b26e68918fa60487e820c9ce0af730ae709
kevinboehmisch <150022344+kevinboehmisch@users.noreply.github.com>
2025-01-18 12:44:55 +0100

    Update Übungsaufgabe_26.sql

--- Softwaretechnik/Datenbanken_2/Lösungen_Laboraufgaben/Übungsaufgabe_26.sql
+++ Softwaretechnik/Datenbanken_2/Lösungen_Laboraufgaben/Übungsaufgabe_26.sql
@@ -2,21 +2,21 @@ SELECT
 	wt.datum, tt.name, wt.spielernr, sp.name, sp.vorname, pr.preisgeld, pr.ausgezahlt_am, 
 	st.strafe, st.spielernr, st.datum
 
-FROM			dbo.Boehmisch_wettkampf AS wt
-	-- INNER JOIN gibt nur ‹bereinstimmungen aus (es werden nur Spieler angezeigt, die an einem Wettkampf teilgenommen haben).
-	INNER JOIN	dbo.Boehmisch_spieler AS sp
+FROM			wettkampf AS wt
+	-- INNER JOIN gibt nur √úbereinstimmungen aus (es werden nur Spieler angezeigt, die an einem Wettkampf teilgenommen haben).
+	INNER JOIN	spieler AS sp
 				ON sp.spielernr=wt.spielernr
-	INNER JOIN	dbo.Boehmisch_team AS tt
+	INNER JOIN	team AS tt
 				ON tt.id = wt.team_id
 
-	-- LEFT JOIN: Zeige alle Wettk‰mpfe, auch wenn kein Preisgeld ausgezahlt wurde.
+	-- LEFT JOIN: Zeige alle Wettk√§mpfe, auch wenn kein Preisgeld ausgezahlt wurde.
 	-- Falls kein Preisgeld existiert, sind die Spalten pr.preisgeld und pr.ausgezahlt_am NULL.
-	LEFT JOIN	dbo.Boehmisch_preisgeld AS pr
+	LEFT JOIN	preisgeld AS pr
 				ON pr.wettkampf_id = wt.id
 
-	-- LEFT JOIN: Zeige auch Wettk‰mpfe ohne Strafen.
-	-- Nur Strafen, die am selben Tag wie der Wettkampf vergeben wurden UND zur gleichen Spieler-Nr. gehˆren, werden angezeigt.
-	LEFT JOIN	dbo.Boehmisch_strafe AS st
+	-- LEFT JOIN: Zeige auch Wettk√§mpfe ohne Strafen.
+	-- Nur Strafen, die am selben Tag wie der Wettkampf vergeben wurden UND zur gleichen Spieler-Nr. geh√∂ren, werden angezeigt.
+	LEFT JOIN	strafe AS st
 				ON st.datum = wt.datum
 				AND st.spielernr = wt.spielernr
 ORDER BY wt.datum DESC;
