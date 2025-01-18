39e38bf5be15d661d247e6a401d6301f31956093
kevinboehmisch <150022344+kevinboehmisch@users.noreply.github.com>
2025-01-18 12:43:52 +0100

    Update Übungsaufgabe_20_komplexe_abfrage.sql

--- Softwaretechnik/Datenbanken_2/Lösungen_Laboraufgaben/Übungsaufgabe_20_komplexe_abfrage.sql
+++ Softwaretechnik/Datenbanken_2/Lösungen_Laboraufgaben/Übungsaufgabe_20_komplexe_abfrage.sql
@@ -1,20 +1,20 @@
 SELECT sp.spielernr, sp.name AS spieler_name, sp.vorname, 
        DATEDIFF(day, sp.geboren, wt.datum) AS tage_seit_geburt,
        tt.name AS team_name, 
-       kap.name AS kapitaen_name,  -- Kapit‰nsname aus 'kap'
+       kap.name AS kapitaen_name,  -- Kapit√§nsname aus 'kap'
        kap.vorname AS kapitaen_vorname
-FROM dbo.Boehmisch_spieler AS sp
-INNER JOIN dbo.Boehmisch_wettkampf AS wt
+FROM spieler AS sp
+INNER JOIN wettkampf AS wt
 ON sp.spielernr = wt.spielernr
-INNER JOIN dbo.Boehmisch_team AS tt
+INNER JOIN team AS tt
 ON wt.team_id = tt.id
--- Neuer Join zur Ermittlung des Kapit‰ns 
-INNER JOIN dbo.Boehmisch_spieler AS kap
+-- Neuer Join zur Ermittlung des Kapit√§ns 
+INNER JOIN spieler AS kap
 ON tt.kapitaen_nr = kap.spielernr
 -- Spieler mit der geringsten Anzahl an Tagen seit Geburt am Wettkampf
 WHERE DATEDIFF(day, sp.geboren, wt.datum) = (
     SELECT MIN(DATEDIFF(day, sp2.geboren, wt2.datum))
-    FROM dbo.Boehmisch_spieler AS sp2
-    INNER JOIN dbo.Boehmisch_wettkampf AS wt2
+    FROM spieler AS sp2
+    INNER JOIN wettkampf AS wt2
     ON sp2.spielernr = wt2.spielernr
 );
