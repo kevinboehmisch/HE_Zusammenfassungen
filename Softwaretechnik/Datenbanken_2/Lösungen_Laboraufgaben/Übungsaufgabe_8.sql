a264ab5d7adb6a9214c2c12a3790cc49e5997e80
kevinboehmisch <150022344+kevinboehmisch@users.noreply.github.com>
2025-01-18 12:45:38 +0100

    Update Übungsaufgabe_8.sql

--- Softwaretechnik/Datenbanken_2/Lösungen_Laboraufgaben/Übungsaufgabe_8.sql
+++ Softwaretechnik/Datenbanken_2/Lösungen_Laboraufgaben/Übungsaufgabe_8.sql
@@ -1,2 +1,2 @@
-SELECT count(spielernr) AS 'Spieler zw. 20 und 30'FROM dbo.Boehmisch_spieler
+SELECT count(spielernr) AS 'Spieler zw. 20 und 30'FROM spieler
 WHERE datediff(yy, geboren, beitritt) BETWEEN 20 AND 30;
