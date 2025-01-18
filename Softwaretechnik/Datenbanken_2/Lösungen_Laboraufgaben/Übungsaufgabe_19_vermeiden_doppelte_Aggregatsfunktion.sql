5ee06e90de6aae36d2e99731df39a199bb2e3d93
kevinboehmisch <150022344+kevinboehmisch@users.noreply.github.com>
2025-01-18 12:43:23 +0100

    Update Übungsaufgabe_19_vermeiden_doppelte_Aggregatsfunktion.sql

--- Softwaretechnik/Datenbanken_2/Lösungen_Laboraufgaben/Übungsaufgabe_19_vermeiden_doppelte_Aggregatsfunktion.sql
+++ Softwaretechnik/Datenbanken_2/Lösungen_Laboraufgaben/Übungsaufgabe_19_vermeiden_doppelte_Aggregatsfunktion.sql
@@ -1,21 +1,21 @@
--- Lösung mit Subquery um doppelte Aggregatsfunktion zu vermeiden
+-- LÃ¶sung mit Subquery um doppelte Aggregatsfunktion zu vermeiden
 SELECT sp.spielernr, sp.name, sp.vorname, SUM(st.strafe) AS gesamtstrafe
-FROM dbo.Boehmisch_spieler AS sp
-INNER JOIN dbo.Boehmisch_strafe AS st
+FROM spieler AS sp
+INNER JOIN strafe AS st
 ON sp.spielernr = st.spielernr
 GROUP BY sp.spielernr, sp.vorname, sp.name
 HAVING SUM(st.strafe) = (
     SELECT MAX(gesamtstrafe)
     FROM (
         SELECT spielernr, SUM(strafe) AS gesamtstrafe
-        FROM dbo.Boehmisch_strafe
+        FROM strafe
         GROUP BY spielernr
     ) AS subquery
 );
 
--- gleiche Lösung aber mit Top 1 um nur das erste Ergebnis der Query auszuwählen
+-- gleiche LÃ¶sung aber mit Top 1 um nur das erste Ergebnis der Query auszuwÃ¤hlen
 SELECT TOP 1 sp.spielernr, sp.name, sp.vorname, SUM(st.strafe)
-FROM dbo.Boehmisch_spieler AS sp
+FROM spieler AS sp
 INNER JOIN dbo.Boehm_strafe AS st
 ON sp.spielernr=st.spielernr
 GROUP BY sp.spielernr, sp.vorname, sp.name
