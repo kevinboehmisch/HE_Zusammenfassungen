dcf4d835f8521d68a1a85ce8de4b98b6f8d8ed81
kevinboehmisch <150022344+kevinboehmisch@users.noreply.github.com>
2025-01-19 20:53:54 +0100

    Update Übungsaufgabe_18.sql

--- Softwaretechnik/Datenbanken_2/Lösungen_Laboraufgaben/Übungsaufgabe_18.sql
+++ Softwaretechnik/Datenbanken_2/Lösungen_Laboraufgaben/Übungsaufgabe_18.sql
@@ -1,6 +1,6 @@
-SELECT TOP 1 sp.spielernr, sp.name, sp.vorname, SUM(st.strafe)
-FROM dbo.Boehmisch_spieler AS sp
-INNER JOIN dbo.Boehm_strafe AS st
-ON sp.spielernr=st.spielernr
-GROUP BY sp.spielernr, sp.vorname, sp.name
-ORDER BY SUM(st.strafe) DESC
+SELECT sp.spielernr, sp.name, sp.vorname
+FROM spieler AS sp
+WHERE sp.spielernr >(
+	SELECT AVG(sp2.spielernr)
+	FROM spieler AS sp2
+)
