767c082204ce8e13618e8c595b5a0722c9e1ab5d
kevinboehmisch <150022344+kevinboehmisch@users.noreply.github.com>
2025-01-19 22:03:33 +0100

    Update Übungsaufgabe_23.sql

--- Softwaretechnik/Datenbanken_2/Lösungen_Laboraufgaben/Übungsaufgabe_23.sql
+++ Softwaretechnik/Datenbanken_2/Lösungen_Laboraufgaben/Übungsaufgabe_23.sql
@@ -1,3 +1,5 @@
+-- hier wichtig man muss sich bewusst sein dass man den Durchschnitt von einer Summe berechnet!
+-- 
 SELECT sp.spielernr, sp.name, sp.vorname, SUM(st.strafe) AS gesamtstrafe
 FROM spieler AS sp
 INNER JOIN strafe AS st
@@ -8,6 +10,7 @@ HAVING SUM(st.strafe) > (
     SELECT AVG(gesamtstrafe2)
     FROM (
 		-- Innere Subquery berechnet die Gesamtsumme der Strafe pro Spieler (pro spielernr)
+		-- Ohne Sum würde nur der Durchschnitt der einzelnen Strafen nicht der Durchschnitt aller Strafen pro Spieler berechnet werden
         SELECT SUM(st2.strafe) AS gesamtstrafe2
         FROM strafe AS st2
 		-- Ohne Group By würde die Summe insgesamt berechnet werden und nicht pro einzelnen Spieler
